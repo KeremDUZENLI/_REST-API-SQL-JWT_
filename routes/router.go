493c865b274aaca684dd320c226d7eca3ac476f7
kerem.duzenli <krmdznl97@gmail.com>
2023-03-14 19:53:01 -0300

    2023.03.14

--- routes/router.go
+++ routes/router.go
@@ -4,6 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type structRoutes struct {
+}
+
+type InterfaceRoutes interface {
+}
+
 func Setup() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
